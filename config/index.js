9cf72e05b9ec5521ea6f706afea4ee0ddcbe7ea6
Mukesh Rawat <mukeshrawat02@gmail.com>
2016-10-28 11:56:41 +0100

    Added production conenction string for mongo

--- config/index.js
+++ config/index.js
@@ -1,6 +1,14 @@
 ﻿(function (config) {
-
-    config.database = 'mongodb://localhost/nodeAPI';
+    var db;
+    if (process.env.NODE_ENV === "development")
+    {
+        db = 'mongodb://localhost/favLister';
+    }
+    else
+    {
+        db = 'mongodb://mgit:admin@123@ds031607.mlab.com:31607/_mgit';
+    }
+    config.database = db;
     config.testDatabase = 'mongodb://localhost/nodeAPI_Test';
     config.secretKey = '12qwaszx!£';
     config.port = process.env.PORT || 3000;
